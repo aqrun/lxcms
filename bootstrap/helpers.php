88df3c173fcff9b2c1ccb32f18dc8814c19c5ea0
Aqrun <aqrun@oicnp.com>
2018-08-23 19:51:08 +0800

    logn modify

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -6,3 +6,7 @@
 function backend_auth(){
     return Auth::guard('admin_guard');
 }
+
+function base_url(){
+    return '/'. config('backend.prefix');
+}
--- resources/views/backend/auth/login.blade.php
+++ resources/views/backend/auth/login.blade.php
@@ -20,10 +20,10 @@
   <div class="login-box-body">
     <p class="login-box-msg">Sign in to start your session</p>
 
-    <form action="../../index2.html" method="post">
+    <form action="{{ base_url() }}/" method="post">
       <div class="form-group has-feedback">
         <input type="text" class="form-control" placeholder="Username">
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        <span class="glyphicon glyphicon-envelope form-control-feedback">dasdas</span>
       </div>
       <div class="form-group has-feedback">
         <input type="password" class="form-control" placeholder="Password">
